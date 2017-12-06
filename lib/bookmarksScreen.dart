581b7a5262545c2e36fdc6a7e6d0efb543c40fd1
Ankur Kedia <ankur@geekyants.com>
2017-12-06 19:13:25 +0530

    Adds library source page
    Adds library source page and adding/removing the sources in firebase

--- lib/bookmarksScreen.dart
+++ lib/bookmarksScreen.dart
@@ -8,162 +8,207 @@ import 'package:firebase_database/ui/firebase_animated_list.dart';
 import './globals.dart' as globals;
 
 class BookmarksScreen extends StatefulWidget {
-	BookmarksScreen({Key key}) : super(key: key);
+  BookmarksScreen({Key key}) : super(key: key);
 
-	@override
-	_BookmarksScreenState createState() => new _BookmarksScreenState();
+  @override
+  _BookmarksScreenState createState() => new _BookmarksScreenState();
 }
 
 class _BookmarksScreenState extends State<BookmarksScreen> {
-	DataSnapshot snapshot;
-	bool change=false;
-	final databaseReference = FirebaseDatabase.instance.reference();
-	var articleDatabaseReference;
-	final FlutterWebviewPlugin flutterWebviewPlugin = new FlutterWebviewPlugin();
+  DataSnapshot snapshot;
+  bool change = false;
+  final databaseReference = FirebaseDatabase.instance.reference();
+  var articleDatabaseReference;
+  final FlutterWebviewPlugin flutterWebviewPlugin = new FlutterWebviewPlugin();
 
-	Future updateSnapshot() async {
-		articleDatabaseReference = databaseReference.child(globals.userId).child('articles');
-		var snap = await articleDatabaseReference.once();
-		this.setState(() {
-			snapshot = snap;
-		});
+  Future updateSnapshot() async {
+    articleDatabaseReference =
+        databaseReference.child(globals.userId).child('articles');
+    var snap = await articleDatabaseReference.once();
+    this.setState(() {
+      snapshot = snap;
+    });
 
-		return "Success!";
-	}
-	@override
-	void initState() {
-		super.initState();
-		this.updateSnapshot();
-	}
-	pushArticle(article){
-		articleDatabaseReference.push().set({
-				'author': article['author'],
-				'description': article['description'],
-				'publishedAt': article['publishedAt'],
-				'title': article['title'],
-				'url': article['url'],
-				'urlToImage': article['urlToImage'],
-			});
-	}
-	_onBookmarkTap(article) {
-		if(snapshot.value!=null) {
-			var value = snapshot.value;
-			int flag=0;
-			value.forEach((k,v) {
-				if( v['url'].compareTo(article['url']) == 0 ){
-					flag=1;
-					articleDatabaseReference.child(k).remove();
-					Scaffold.of(context).showSnackBar(new SnackBar(
-						content: new Text('Bookmark removed'),
-						backgroundColor: Colors.grey[600],
-					));
-				}
-			});
-			if(flag != 1){
-				pushArticle(article);
-				Scaffold.of(context).showSnackBar(new SnackBar(
-					content: new Text('Bookmark added'),
-					backgroundColor: Colors.grey[600],
-				));
-			}
-			this.setState(() {change = true;});
-		} else {
-			pushArticle(article);
-		}
-	}
+    return "Success!";
+  }
 
-	Column buildButtonColumn(IconData icon) {
-		Color color = Theme.of(context).primaryColor;
-		return new Column(
-			mainAxisSize: MainAxisSize.min,
-			mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-			children: [
-				new Icon(icon, color: color),
-			],
-		);
-	}
-	@override
-	Widget build(BuildContext context) {
-		return new Scaffold(
-			body: new Column(children: <Widget>[
-			new Flexible(
-			child: new FirebaseAnimatedList(
-				query: articleDatabaseReference,
-				sort: (a, b) => b.key.compareTo(a.key),
-				padding: new EdgeInsets.all(8.0),
-				itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation, int index) {
-					return new GestureDetector(
-						child: new Card(
-							child: new Row(
-								children: [
-									new Expanded(
-										child: new GestureDetector(
-											child: new Column(
-												crossAxisAlignment: CrossAxisAlignment.start,
-												children: [
-													new Text(
-														snapshot.value["title"],
-														style: new TextStyle(
-															fontWeight: FontWeight.bold,
-														),
-													),
-													new Text(
-														snapshot.value["description"],
-														style: new TextStyle(
-															color: Colors.grey[500],
-														),
-													),
-													new Padding(
-															padding: new EdgeInsets.all(5.0),
-															child: snapshot.value["author"] != null?
-																new Text( "Author: ${snapshot.value["author"]}",
-																	style: new TextStyle(
-																			fontWeight: FontWeight.w500,
-																			color: Colors.black,
-																		),
-																):
-																null
-													),
-												],
-											),
-											onTap: () {
-												flutterWebviewPlugin.launch(snapshot.value["url"], fullScreen: false);
-											},
-										),
-									),
-									new Column(
-										children: <Widget>[
-											new SizedBox(
-												height: 100.0,
-												width: 100.0,
-												child: new Image.network(
-													snapshot.value["urlToImage"],
-													fit: BoxFit.cover,
-												),
-											),
-											new Row(
-												children: <Widget>[
-													new GestureDetector(
-														child: buildButtonColumn(Icons.share),
-														onTap: () {share(snapshot.value["url"]);},
-													),
-													new GestureDetector(
-														child: buildButtonColumn(Icons.bookmark),
-														onTap: () { _onBookmarkTap( snapshot.value);},
-													),
-												],
-											)
-										],
-									),
-								],
-							),
-						),
-					);
-				},
-			),
-		),
-		],
-			),
-		);
-	}
+  @override
+  void initState() {
+    super.initState();
+    this.updateSnapshot();
+  }
+
+  pushArticle(article) {
+    articleDatabaseReference.push().set({
+      'author': article['author'],
+      'description': article['description'],
+      'publishedAt': article['publishedAt'],
+      'title': article['title'],
+      'url': article['url'],
+      'urlToImage': article['urlToImage'],
+    });
+  }
+
+  _onBookmarkTap(article) {
+    if (snapshot.value != null) {
+      var value = snapshot.value;
+      int flag = 0;
+      value.forEach((k, v) {
+        if (v['url'].compareTo(article['url']) == 0) {
+          flag = 1;
+          articleDatabaseReference.child(k).remove();
+          Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text('Bookmark removed'),
+                backgroundColor: Colors.grey[600],
+              ));
+        }
+      });
+      if (flag != 1) {
+        pushArticle(article);
+        Scaffold.of(context).showSnackBar(new SnackBar(
+              content: new Text('Bookmark added'),
+              backgroundColor: Colors.grey[600],
+            ));
+      }
+      this.updateSnapshot();
+      this.setState(() {
+        change = true;
+      });
+    }
+  }
+
+  Column buildButtonColumn(IconData icon) {
+    Color color = Theme.of(context).primaryColor;
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        new Icon(icon, color: color),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: (snapshot != null && snapshot.value != null)
+          ? new Column(
+              children: <Widget>[
+                new Flexible(
+                    child: new FirebaseAnimatedList(
+                  query: articleDatabaseReference,
+                  sort: (a, b) => b.key.compareTo(a.key),
+                  padding: new EdgeInsets.all(8.0),
+                  itemBuilder: (_, DataSnapshot snapshot,
+                      Animation<double> animation, int index) {
+                    return new GestureDetector(
+                      child: new Card(
+                        child: new Row(
+                          children: [
+                            new Expanded(
+                              child: new GestureDetector(
+                                child: new Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    new Text(
+                                      snapshot.value["title"],
+                                      style: new TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    new Text(
+                                      snapshot.value["description"],
+                                      style: new TextStyle(
+                                        color: Colors.grey[500],
+                                      ),
+                                    ),
+                                    new Padding(
+                                        padding: new EdgeInsets.all(5.0),
+                                        child: snapshot.value["author"] != null
+                                            ? new Text(
+                                                "Author: ${snapshot.value["author"]}",
+                                                style: new TextStyle(
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Colors.black,
+                                                ),
+                                              )
+                                            : null),
+                                  ],
+                                ),
+                                onTap: () {
+                                  flutterWebviewPlugin.launch(
+                                      snapshot.value["url"],
+                                      fullScreen: false);
+                                },
+                              ),
+                            ),
+                            new Column(
+                              children: <Widget>[
+                                new SizedBox(
+                                  height: 100.0,
+                                  width: 100.0,
+                                  child: new Image.network(
+                                    snapshot.value["urlToImage"],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                new Row(
+                                  children: <Widget>[
+                                    new GestureDetector(
+                                      child: buildButtonColumn(Icons.share),
+                                      onTap: () {
+                                        share(snapshot.value["url"]);
+                                      },
+                                    ),
+                                    new GestureDetector(
+                                      child: buildButtonColumn(Icons.bookmark),
+                                      onTap: () {
+                                        _onBookmarkTap(snapshot.value);
+                                      },
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )),
+              ],
+            )
+          : new Center(
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  new Icon(Icons.bookmark_border,
+                      color: Colors.grey, size: 60.0),
+                  new Text(
+                    "No Bookmarks",
+                    style: new TextStyle(fontSize: 24.0, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
 }
+
+//For empty bookmark screen add this
+// (snapshot != null && snapshot.value != null)
+// : new Center(
+//                     child: new Column(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         new Icon(Icons.bookmark_border,
+//                             color: Colors.grey, size: 60.0),
+//                         new Text(
+//                           "No Bookmarks",
+//                           style:
+//                               new TextStyle(fontSize: 24.0, color: Colors.grey),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -10,235 +10,226 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import './globals.dart' as globals;
 
 class HomeScreen extends StatefulWidget {
-	HomeScreen({Key key}) : super(key: key);
+  HomeScreen({Key key}) : super(key: key);
 
-	@override
-	_HomeScreenState createState() => new _HomeScreenState();
+  @override
+  _HomeScreenState createState() => new _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-	var data;
-	var sources;
-	var user;
-	bool change= false;
-	var newsSelection = "techcrunch";
-	DataSnapshot snapshot;
-	final FlutterWebviewPlugin flutterWebviewPlugin = new FlutterWebviewPlugin();
-	final auth = FirebaseAuth.instance;
-	final databaseReference = FirebaseDatabase.instance.reference();
-	var userDatabaseReference;
-	var articleDatabaseReference;
+  var data;
+  var sources;
+  var user;
+  bool change = false;
+  var newsSelection = "techcrunch";
+  DataSnapshot snapshot;
+  final FlutterWebviewPlugin flutterWebviewPlugin = new FlutterWebviewPlugin();
+  final auth = FirebaseAuth.instance;
+  final databaseReference = FirebaseDatabase.instance.reference();
+  var userDatabaseReference;
+  var articleDatabaseReference;
 
-	// Future<Null> _ensureLoggedIn() async {
-	// 	GoogleSignInAccount user = googleSignIn.currentUser;
-	// 	if (user == null)
-	// 		user = await googleSignIn.signInSilently();
-	// 	if (user == null) {
-	// 		await googleSignIn.signIn();
-	// 		analytics.logLogin();
-	// 	}
-	// 	if (await auth.currentUser() == null) {
-	// 		GoogleSignInAuthentication credentials = await googleSignIn.currentUser.authentication;
-	// 		await auth.signInWithGoogle(
-	// 			idToken: credentials.idToken,
-	// 			accessToken: credentials.accessToken,
-	// 		);
-	// 	}
-	// 	globals.userId = user.id;
-	// 	//set user id from user and then map it to firebase database
-	// }
-	Future getData() async {
-		var response = await http.get(Uri.encodeFull('http://newsapi.org/v1/articles?source='+newsSelection+'&sortBy=top&apiKey=ab31ce4a49814a27bbb16dd5c5c06608'),
-			headers: {
-				"Accept": "application/json"
-			}
-		);
-		var responseSources = await http.get(Uri.encodeFull('https://newsapi.org/v1/sources?language=en'),
-			headers: {
-				"Accept": "application/json"
-			}
-		);
-		userDatabaseReference = databaseReference.child(globals.userId);
-		articleDatabaseReference = userDatabaseReference.child('articles');
-		var snap = await articleDatabaseReference.once();
-		this.setState(() {
-			data = JSON.decode(response.body);
-			sources = JSON.decode(responseSources.body);
-			snapshot = snap;
-		});
-		return "Success!";
-	}
-	_hasArticle(article) {
-		if(snapshot.value!=null){
-			var value = snapshot.value;
-			int flag=0;
-			if(value!= null){
-				value.forEach((k,v){
-					if( v['url'].compareTo(article['url']) == 0 ){
-						flag=1;
-						// return true;
-					}
-				});
-				if(flag==1)
-					return true;
-				else
-					return false;
-			}
-		}
-		return false;
-	}
-	pushArticle(article){
-		print("push article called");
-		articleDatabaseReference.push().set({
-				'author': article['author'],
-				'description': article['description'],
-				'publishedAt': article['publishedAt'],
-				'title': article['title'],
-				'url': article['url'],
-				'urlToImage': article['urlToImage'],
-			});
-	}
-	_onBookmarkTap(article) {
-		print("Bookmark tap called");
-		if(snapshot.value!=null) {
-			var value = snapshot.value;
-			int flag=0;
-			value.forEach((k,v) {
-				if( v['url'].compareTo(article['url']) == 0 ){
-					flag=1;
-					articleDatabaseReference.child(k).remove();
-					Scaffold.of(context).showSnackBar(new SnackBar(
-						content: new Text('Bookmark removed'),
-						backgroundColor: Colors.grey[600],
-					));
-				}
-			});
-			if(flag != 1){
-				pushArticle(article);
-				Scaffold.of(context).showSnackBar(new SnackBar(
-					content: new Text('Bookmark added'),
-					backgroundColor: Colors.grey[600],
-				));
-			}
-		} else {
-				pushArticle(article);
-		}
-		this.setState(() {change = true;});
-	}
-	_findSourceName(id) {
-		for( var i in sources["sources"]){
-			if( i["id"].compareTo(id) == 0 )
-				return i["name"];
-		}
-	}
-	_refresh(){
-		this.getData();
-	}
+  Future getData() async {
+    var response = await http.get(
+        Uri.encodeFull('http://newsapi.org/v1/articles?source=' +
+            newsSelection +
+            '&sortBy=top&apiKey=ab31ce4a49814a27bbb16dd5c5c06608'),
+        headers: {"Accept": "application/json"});
+    var responseSources = await http.get(
+        Uri.encodeFull('https://newsapi.org/v1/sources?language=en'),
+        headers: {"Accept": "application/json"});
+    userDatabaseReference = databaseReference.child(globals.userId);
+    articleDatabaseReference = userDatabaseReference.child('articles');
+    var snap = await articleDatabaseReference.once();
+    this.setState(() {
+      data = JSON.decode(response.body);
+      sources = JSON.decode(responseSources.body);
+      snapshot = snap;
+    });
+    return "Success!";
+  }
 
-	@override
-	void initState() {
-		super.initState();
-		// _ensureLoggedIn();
-		this.getData();
-	}
+  _hasArticle(article) {
+    if (snapshot.value != null) {
+      var value = snapshot.value;
+      int flag = 0;
+      if (value != null) {
+        value.forEach((k, v) {
+          if (v['url'].compareTo(article['url']) == 0) {
+            flag = 1;
+            // return true;
+          }
+        });
+        if (flag == 1) return true;
+      }
+    }
+    return false;
+  }
 
-	Column buildButtonColumn(IconData icon) {
-		Color color = Theme.of(context).primaryColor;
+  pushArticle(article) {
+    articleDatabaseReference.push().set({
+      'author': article['author'],
+      'description': article['description'],
+      'publishedAt': article['publishedAt'],
+      'title': article['title'],
+      'url': article['url'],
+      'urlToImage': article['urlToImage'],
+    });
+  }
 
-		return new Column(
-			mainAxisSize: MainAxisSize.min,
-			mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-			children: [
-				new Icon(icon, color: color),
-			],
-		);
-	}
+  _onBookmarkTap(article) {
+    if (snapshot.value != null) {
+      var value = snapshot.value;
+      int flag = 0;
+      value.forEach((k, v) {
+        if (v['url'].compareTo(article['url']) == 0) {
+          flag = 1;
+          articleDatabaseReference.child(k).remove();
+          Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text('Bookmark removed'),
+                backgroundColor: Colors.grey[600],
+              ));
+        }
+      });
+      if (flag != 1) {
+        Scaffold.of(context).showSnackBar(new SnackBar(
+              content: new Text('Bookmark added'),
+              backgroundColor: Colors.grey[600],
+            ));
+        pushArticle(article);
+      }
+    } else {
+      pushArticle(article);
+    }
+    this.getData();
+    this.setState(() {
+      change = true;
+    });
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return new Scaffold(
-			backgroundColor: Colors.grey[200],
-			body: new GestureDetector(
-				child: data==null? const Center(
-					child: const CupertinoActivityIndicator(),
-				):
-				new ListView.builder(
-					itemCount: data == null ? 0 : data.length,
-					itemBuilder: (BuildContext context, int index) {
-						return new GestureDetector(
-							child: new Card(
-								child: new Row(
-									children: [ 
-										new Expanded(
-											child: new GestureDetector(
-												child: new Column(
-													crossAxisAlignment: CrossAxisAlignment.start,
-													children: [
-														new Text(
-															data["articles"][index]["title"],
-															style: new TextStyle(
-																fontWeight: FontWeight.bold,
-															),
-														),
-														new Text(
-															data["articles"][index]["description"],
-															style: new TextStyle(
-																color: Colors.black,
-															),
-														),
-														new Row(
-															crossAxisAlignment: CrossAxisAlignment.end,
-															children: <Widget>[
-																new Text(
-																	"Source: ${_findSourceName(data["source"])}",
-																	style: new TextStyle(
-																			fontWeight: FontWeight.w500,
-																			color: Colors.black,
-																		),
-																),
-															],
-														),
-													],
-												),
-												onTap: () {
-													flutterWebviewPlugin.launch(data["articles"][index]["url"], fullScreen: false);
-												},
-											),
-										),
-										new Column(
-											children: <Widget>[
-												new SizedBox(
-													height: 100.0,
-													width: 100.0,
-													child: new Image.network(
-														data["articles"][index]["urlToImage"],
-														fit: BoxFit.cover,
-													),
-												),
-												new Row(
-													children: <Widget>[
-														new GestureDetector(
-															child: buildButtonColumn(Icons.share),
-															onTap: () { share(data["articles"][index]["url"]);},
-														),
-														new GestureDetector(
-															child: _hasArticle(data["articles"][index])? buildButtonColumn(Icons.bookmark):buildButtonColumn(Icons.bookmark_border) ,
-															onTap: () { _onBookmarkTap( data["articles"][index]);},
-														),
-													],
-												)
-											],
-										)
-									],
-								),
-							),
-						);
-					},
-				),
-				onVerticalDragDown: _refresh(),
-			),
-		);
-	}
+  _findSourceName(id) {
+    for (var i in sources["sources"]) {
+      if (i["id"].compareTo(id) == 0) return i["name"];
+    }
+  }
+
+  _refresh() {
+    this.getData();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getData();
+  }
+
+  Column buildButtonColumn(IconData icon) {
+    Color color = Theme.of(context).primaryColor;
+
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        new Icon(icon, color: color),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: new GestureDetector(
+        child: data == null
+            ? const Center(
+                child: const CupertinoActivityIndicator(),
+              )
+            : new ListView.builder(
+                itemCount: data == null ? 0 : data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new GestureDetector(
+                    child: new Card(
+                      child: new Row(
+                        children: [
+                          new Expanded(
+                            child: new GestureDetector(
+                              child: new Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  new Text(
+                                    data["articles"][index]["title"],
+                                    style: new TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  new Text(
+                                    data["articles"][index]["description"],
+                                    style: new TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  new Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: <Widget>[
+                                      new Text(
+                                        "Source: ${_findSourceName(data["source"])}",
+                                        style: new TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              onTap: () {
+                                flutterWebviewPlugin.launch(
+                                    data["articles"][index]["url"],
+                                    fullScreen: false);
+                              },
+                            ),
+                          ),
+                          new Column(
+                            children: <Widget>[
+                              new SizedBox(
+                                height: 100.0,
+                                width: 100.0,
+                                child: new Image.network(
+                                  data["articles"][index]["urlToImage"],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              new Row(
+                                children: <Widget>[
+                                  new GestureDetector(
+                                    child: buildButtonColumn(Icons.share),
+                                    onTap: () {
+                                      share(data["articles"][index]["url"]);
+                                    },
+                                  ),
+                                  new GestureDetector(
+                                    child: _hasArticle(data["articles"][index])
+                                        ? buildButtonColumn(Icons.bookmark)
+                                        : buildButtonColumn(
+                                            Icons.bookmark_border),
+                                    onTap: () {
+                                      _onBookmarkTap(data["articles"][index]);
+                                    },
+                                  ),
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+        onVerticalDragDown: _refresh(),
+      ),
+    );
+  }
 }
 
-//find something related to bind and use function _hasArticle with it. RIght now it keeps on rendering 
+//find something related to bind and use function _hasArticle with it. RIght now it keeps on rendering
